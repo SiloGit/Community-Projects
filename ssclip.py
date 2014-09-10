3b7bbfb8880ec71dcbde3d97bcc94b3a2c725702
Projects by Silo Collective <silogit@silosec.org>
2014-09-10 16:16:52 -0400

    Update ssclip.py

--- ssclip.py
+++ ssclip.py
@@ -5,8 +5,7 @@
 #	SiloSec presents - 
 #	Our first community project 
 
-"""
-                                                                              
+"""                                                                           
 d888888b d8b   db d88888b  .d88b.  .d8888. d88888b  .o88b. 
   `88'   888o  88 88'     .8P  Y8. 88'  YP 88'     d8P  Y8 
    88    88V8o 88 88ooo   88    88 `8bo.   88ooooo 8P      
@@ -34,18 +33,16 @@ Y888888P VP   V8P YP       `Y88P'  `8888Y' Y88888P  `Y88P'
 	
 	* Add HMAC for overkill safety and integrity check
 	* 
-
-
 """
 
 __authors__ = "Jay (SiloSec) (j@silosec.org), jsrn - #slothkrew"
-__version__ = "0.1a"
+__version__ = "0.2a"
 __date__ = "Date: Sep 10, 2014"
 __more_info__ = "http://www.silosec.org"
 
 
 ### Imports ----
-import urllib3 #or...
+import requests #or...
 from mechanize import Browser		# Fetches info for BSoup
 import re 							# Regular Expressions - Used here to organize strings
 import sys							# For Command-Line Arguments and Script Exit
@@ -65,14 +62,14 @@ P  = '\033[35m' # purple
 GR = '\033[37m' # gray
 
 fcurl = "http://quizlet.com/44970397/linux-and-hacking-common-commands-and-memorize-mes-flash-cards/"  # Location of the Quizlet Flash Cards
+wurl = "http://www.silosec.org/w"	# Wiki's URL
 
 mech = Browser()					# Using Mechanize to fetch html
 page = mech.open(fcurl)				# Assigning the fetched page
 html = page.read()					# HTML Content ready for BS
-wurl = ""							# Wiki's URL
 
-user = raw_input("Enter Account Username: ")
-pwd = getpass.getpass("Enter Account password: ")
+soup = BeautifulSoup(html)
+sort = (soup.get_text())
 							
 version = "0.1a"					# Script Version Number
 
@@ -81,6 +78,37 @@ version = "0.1a"					# Script Version Number
 ### Main ----
 
 print("\x1B]0;---InfoSec Flashcards--- \x07")	# Print to Terminal Title Bar
+print G+'*'*80
+print P+"                            You're using"
+print ""
+print ""
+
+print "         d888888b d8b   db d88888b  .d88b.  .d8888. d88888b  .o88b. "
+print "           `88'   888o  88 88'     .8P  Y8. 88'  YP 88'     d8P  Y8 "
+print "            88    88V8o 88 88ooo   88    88 `8bo.   88ooooo 8P      "
+print "            88    88 V8o88 88~~~   88    88   `Y8b. 88~~~~~ 8b      "
+print "           .88.   88  V888 88      `8b  d8' db   8D 88.     Y8b  d8 "
+print "         Y888888P VP   V8P YP       `Y88P'  `8888Y' Y88888P  `Y88P' "
+print ""
+print "               ______ _           _                       _      "
+print "              |  ____| |         | |                     | |     "
+print "              | |__  | | __ _ ___| |__   ___ __ _ _ __ __| |___  "
+print "              |  __| | |/ _` / __| '_ \ / __/ _` | '__/ _` / __| "
+print "              | |    | | (_| \__ \ | | | (_| (_| | | | (_| \__ \ "
+print "              |_|    |_|\__,_|___/_| |_|\___\__,_|_|  \__,_|___/ "
+                                                        
+
+print ""
+print ""
+print P+"                       A Silo Collective project"
+print ""
+print P+"                   Please login to the Quizlet API key: "
+print ""
+print ""
+
+# Fetch credentials from the user - 
+user = raw_input(G+"Enter Account Username: ")
+pwd = getpass.getpass(G+"Enter Account password: ")
 
 # Help menu
 def usage():
